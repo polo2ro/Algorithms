58bbe5f9f841c95fbb8639284e58621f9132a80c
Paul de Rosanbo <paul@rosanbo.com>
2016-11-18 23:00:44 +0100

    test

--- src/trie.hs
+++ src/trie.hs
@@ -3,19 +3,21 @@ import Debug.Trace
 findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
 findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
 
-drillDict :: (String, [(Char, String)]) -> Maybe String
+drillDict :: (String, [(Char, String)]) -> [(Char, String)]
 drillDict t = do
     let wordd = fst t
     let letter = head wordd
     let dict = snd t
-    trace ("wordd: " ++ show wordd ++ ", letter: " ++ show letter) (findKey letter dict)
+    let value = findKey letter dict
+    if isNothing value then (letter,[wordd]):dict else wordd:value
+    return dict
 
 
-
-buildTrie :: [String] -> [Maybe String]
+buildTrie :: [String] -> [(Char, String)]
 buildTrie words = do
     let dict = []
     map drillDict [(w, dict) | w <- words]
+    return dict
 
 isWordInTrie word = word
 
