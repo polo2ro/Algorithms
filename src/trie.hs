077e700db8aa9647ef2ff5cf419689e0d101453d
Paul de Rosanbo <paul@rosanbo.com>
2016-11-18 21:17:03 +0100

    First try with Debug.trace

--- src/trie.hs
+++ src/trie.hs
@@ -1,12 +1,26 @@
+import Debug.Trace
 
-generateTrieFromWords words = do
-    let root = ()
-    let wordx = head words
-    let wordy = drop 1 wordx
-    wordy
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
+
+drillDict :: (String, [(Char, String)]) -> Maybe String
+drillDict t = do
+    let wordd = fst t
+    let letter = head wordd
+    let dict = snd t
+    trace ("wordd: " ++ show wordd ++ ", letter: " ++ show letter) (findKey letter dict)
+
+
+
+buildTrie :: [String] -> [Maybe String]
+buildTrie words = do
+    let dict = []
+    map drillDict [(w, dict) | w <- words]
+
+isWordInTrie word = word
 
 testlist = ["hello", "hey", "what", "when", "why"]
 
 main = do
-    let trie = generateTrieFromWords testlist
+    let trie = buildTrie testlist
     print trie
