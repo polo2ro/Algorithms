2c12bd96cb9f16b47a7ffa6780e93ca5d05c746b
Paul de Rosanbo <paul@rosanbo.com>
2016-11-19 15:06:41 +0100

    more and more errors

--- src/trie.hs
+++ src/trie.hs
@@ -1,23 +1,39 @@
 import Debug.Trace
 
-findKey :: (Eq k) => k -> [(k,v)] -> Maybe v
+-- Get the list of LetterDict associated to letter
+findKey :: (Eq k) => k -> LetterDict -> Maybe [LetterDict]
 findKey key = foldr (\(k,v) acc -> if key == k then Just v else acc) Nothing
 
-drillDict :: (String, [(Char, String)]) -> [(Char, String)]
-drillDict t = do
-    let wordd = fst t
-    let letter = head wordd
-    let dict = snd t
-    let value = findKey letter dict
-    if isNothing value then (letter,[wordd]):dict else wordd:value
-    return dict
+-- The LetterDict data type contain on level of trie
+-- dict 1 ex: ("a", [])
+-- dict 1 and dict2 ex: ("a", [("b", [])])
+data LetterDict = LetterDict Char [LetterDict] deriving (Show)
+
+-- Argument for drillDict
+data LevelArg = LevelArg { word :: String, dict :: [LetterDict], position :: Int } deriving (Show)
+
+
+-- Create new LevelArg for the next position in word
+nextlevel :: LevelArg -> LevelArg
+nextlevel level =
+    LevelArg (word level) (dict level) (position level +1)
+
+-- Add letter and recursive dicts to dict
+drillDict :: LevelArg -> [LetterDict]
+drillDict level =
+    let letter = word level !! position level
+        letterDictList = findKey letter $ dict level
+    in
+        if isNothing letterDictList
+            then LetterDict letter (drillDict $ nextlevel level):dict level
+            else word:letterDictList
+    letterDictList
 
 
 buildTrie :: [String] -> [(Char, String)]
-buildTrie words = do
-    let dict = []
-    map drillDict [(w, dict) | w <- words]
-    return dict
+buildTrie words =
+    let dict = [] -- This is a list of LetterDict
+    in map drillDict [(w, dict, 0) | w <- words]
 
 isWordInTrie word = word
 
