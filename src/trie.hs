2ae5ca89699162db5fb7231fd3c55337c35694e0
Paul de Rosanbo <paul@rosanbo.com>
2016-11-20 11:47:24 +0100

    recursion

--- src/trie.hs
+++ src/trie.hs
@@ -1,44 +1,44 @@
-import Debug.Trace
-
-
 -- The LetterDict data type contain on level of trie
 -- LetterDict a (LetterDict b (LetterDict c Nothing))
 data LetterDict = LetterDict { letter :: Char, dictlist :: [LetterDict] } deriving (Show)
 
 
 -- Get the list of LetterDict associated to letter
-findKey :: (Eq k) => k -> [LetterDict] -> Maybe [LetterDict]
+findKey :: Char -> [LetterDict] -> Maybe [LetterDict]
 findKey key [] = Nothing
-findKey key xs = foldr (\letterDict
-                    -> if key == letter letterDict then
-                        Just (dictlist letterDict)
-                    else
-                        letterDict)
-                    Nothing xs
-
-
-
-{-|
+findKey key (letterDict:xs) = if key == letter letterDict
+                                then Just (dictlist letterDict)
+                                else findKey key xs
 
 -- Argument for drillDict
-data LevelArg = LevelArg { word :: String, dict :: [Maybe LetterDict], position :: Int } deriving (Show)
+data LevelArg = LevelArg { word :: String, leveldl :: [LetterDict], position :: Int } deriving (Show)
+
+
 
 
 -- Create new LevelArg for the next position in word
 nextlevel :: LevelArg -> LevelArg
 nextlevel level =
-    LevelArg (word level) (dict level) (position level +1)
+    LevelArg (word level) (leveldl level) (position level +1)
+
+
 
 -- Add letter and recursive dicts to dict
 drillDict :: LevelArg -> [LetterDict]
 drillDict level =
     let letter = word level !! position level
-        letterDictList = findKey letter $ dict level
+        leveldlLocal = leveldl level
+        letterDictList = findKey letter leveldlLocal
+        insert = LetterDict letter (drillDict $ nextlevel level)
     in
-        if isNothing letterDictList
-            then LetterDict letter (drillDict $ nextlevel level):dict level
-            else word:letterDictList
-    letterDictList
+        if Nothing == letterDictList
+            then insert : leveldl level     -- insert a LetterDict in the list of this level
+            else letterDictList             -- insert a LetterDict for the found letter
+
+{-|
+
+
+
 
 
 buildTrie :: [String] -> [(Char, String)]
@@ -48,9 +48,11 @@ buildTrie words =
 
 isWordInTrie word = word
 
+-}
+
 testlist = ["hello", "hey", "what", "when", "why"]
 
--}
+
 
 testtrie = [
         LetterDict 'a' [
@@ -60,6 +62,9 @@ testtrie = [
                         LetterDict 'e' []
                     ]
                 ]
+            ],
+            LetterDict 's' [
+                LetterDict 's' []
             ]
         ]
     ]
