ed757d24d5bc7739be50c9b598f7e6e8e29a76e5
Paul de Rosanbo <paul@rosanbo.com>
2017-04-26 21:22:31 +0200

    Sort by height

---
+++ src/queue.hs
@@ -0,0 +1,38 @@
+-- Suppose you have a random list of people standing in a queue.
+-- Each person is described by a pair of integers (h, k),
+-- where h is the height of the person and k is the number of people
+-- in front of this person who have a height greater than or equal to h.
+-- Write an algorithm to reconstruct the queue.
+
+-- Note:
+-- The number of people is less than 1,100.
+
+-- Example
+
+-- Input:
+-- [(7,0), (4,4), (7,1), (5,0), (6,1), (5,2)]
+
+-- Output:
+-- [(5,0), (7,0), (5,2), (6,1), (4,4), (7,1)]
+
+
+filterHeight :: (Int -> Bool) -> [(Int, Int)] -> [(Int, Int)]
+filterHeight _ [] = []
+filterHeight p (x:xs)
+    | p (fst x) = x : filterHeight p xs
+    | otherwise = filterHeight p xs
+    
+
+reconstruct :: [(Int, Int)] -> [(Int, Int)]
+reconstruct [] = []
+reconstruct (x:xs) =
+        let
+        smallerSorted = reconstruct (filterHeight (<=fst x) xs)
+        biggerSorted = reconstruct (filterHeight (>fst x) xs)
+    in  smallerSorted ++ [x] ++ biggerSorted
+
+
+
+
+main :: IO()
+main = print (reconstruct [(7,0), (4,4), (7,1), (5,0), (6,1), (5,2)])
