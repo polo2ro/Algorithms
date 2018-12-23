99c951659577aca2b6eb18859d16db513793d794
Paul de Rosanbo <paul@rosanbo.com>
2018-12-23 16:10:12 +0100

    keyword row algo

---
+++ src/find_keyboard_row.hs
@@ -0,0 +1,19 @@
+-- Given a List of words, return the words that can be typed using letters of
+-- alphabet on only one row's of American keyboard.
+-- https://github.com/keon/algorithms/blob/master/algorithms/set/find_keyboard_row.py
+-- https://leetcode.com/problems/keyboard-row/description/
+
+import Data.List
+import Data.Maybe
+import Data.Char
+
+checkRow :: String -> String -> Bool
+checkRow word row = search == search `intersect` row
+    where search = map toLower word
+
+isInOneKeyboardRow :: String -> Bool
+isInOneKeyboardRow word = isJust (find (checkRow word) keyboard)
+    where keyboard = ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+
+main :: IO()
+main = print (filter isInOneKeyboardRow ["Hello", "Alaska", "Dad", "Peace"])
